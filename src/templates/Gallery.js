7c0f8a7a1fee0c42574be18cb4a3a784f849d2d9
Jayspart45 <91474610+Jayspart45@users.noreply.github.com>
2022-10-14 18:48:41 +0530

    Add files via upload

---
+++ src/templates/Gallery.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import image1 from './images/img2.jpeg';
+export default function Gallery() {
+    return (
+        <div className="Gallery ">
+            <div className="row">
+                <div className="card">
+                    <img src={image1} className="card-img-top img-fluid" alt="..." />
+                    <div className="card-body">
+                        <h3 className="card-title text-center">Title</h3>
+
+                        <p className="card-text">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam maxime
+                            deserunt natus saepe modi vel, quod officia voluptate libero sit
+                            reprehenderit voluptatibus ullam beatae, ipsum ratione eum dicta quis
+                            optio.
+                        </p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src={image1} className="card-img-top img-fluid" alt="..." />
+                    <div className="card-body">
+                        <h3 className="card-title text-center">Title</h3>
+
+                        <p className="card-text">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam maxime
+                            deserunt natus saepe modi vel, quod officia voluptate libero sit
+                            reprehenderit voluptatibus ullam beatae, ipsum ratione eum dicta quis
+                            optio.
+                        </p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src={image1} className="card-img-top img-fluid" alt="..." />
+                    <div className="card-body">
+                        <h3 className="card-title text-center">Title</h3>
+
+                        <p className="card-text">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam maxime
+                            deserunt natus saepe modi vel, quod officia voluptate libero sit
+                            reprehenderit voluptatibus ullam beatae, ipsum ratione eum dicta quis
+                            optio.
+                        </p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src={image1} className="card-img-top img-fluid" alt="..." />
+                    <div className="card-body">
+                        <h3 className="card-title text-center">Title</h3>
+
+                        <p className="card-text">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam maxime
+                            deserunt natus saepe modi vel, quod officia voluptate libero sit
+                            reprehenderit voluptatibus ullam beatae, ipsum ratione eum dicta quis
+                            optio.
+                        </p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src={image1} className="card-img-top img-fluid" alt="..." />
+                    <div className="card-body">
+                        <h3 className="card-title text-center">Title</h3>
+
+                        <p className="card-text">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam maxime
+                            deserunt natus saepe modi vel, quod officia voluptate libero sit
+                            reprehenderit voluptatibus ullam beatae, ipsum ratione eum dicta quis
+                            optio.
+                        </p>
+                    </div>
+                </div>
+                <div className="card">
+                    <img src={image1} className="card-img-top img-fluid" alt="..." />
+                    <div className="card-body">
+                        <h3 className="card-title text-center">Title</h3>
+
+                        <p className="card-text">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam maxime
+                            deserunt natus saepe modi vel, quod officia voluptate libero sit
+                            reprehenderit voluptatibus ullam beatae, ipsum ratione eum dicta quis
+                            optio.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
